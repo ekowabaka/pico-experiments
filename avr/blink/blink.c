626872a423a3e9a25bfdeb1fd8961018b487a0f8
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-02-05 23:34:47 -0600

    Experimenting with AVR blinking

---
+++ avr/blink/blink.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+
+int main()
+{
+    DDRB |= 0b00100000;
+
+    while (1)
+    {        
+        PORTB |= 0b00100000;
+
+        for (long i = 0; i < 1000; i++)
+        {
+            PORTB |= 0b00100000;
+        }
+        PORTB &= 0b11011111;
+        for (long i = 0; i < 100000; i++)
+        {
+            PORTB &= 0b11011111;
+        }
+    }
+}
